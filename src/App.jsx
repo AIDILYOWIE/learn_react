5faf7d3e32f4b1a03bce9bb9f8edfc20357555fa
aidil <yojesjasteb@gmail.com>
2025-04-11 19:51:39 +0700

    implementasi atomic design

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import { Button } from './Components/Elements/Button/Index'
+
+function App() {
+  return (
+    <Button variant='bg-red'>buy</Button>
+  )
+}
+
+export default App
